cabe446e648319bf03bede6fc49090d777f0d3f4
fijiro <jiro.protonmail@protonmail.com>
2024-03-14 18:45:55 +0200

    Initial

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module iiro.toiv.mandelbrotjavafx {
+    requires javafx.controls;
+
+    opens iiro.toiv.mandelbrotjavafx;
+    exports iiro.toiv.mandelbrotjavafx;
+    exports iiro.toiv.mandelbrotjavafx.Graphics;
+    opens iiro.toiv.mandelbrotjavafx.Graphics;
+    exports iiro.toiv.mandelbrotjavafx.Input;
+    opens iiro.toiv.mandelbrotjavafx.Input;
+    exports iiro.toiv.mandelbrotjavafx.Positions;
+    opens iiro.toiv.mandelbrotjavafx.Positions;
+    exports iiro.toiv.mandelbrotjavafx.tests;
+    opens iiro.toiv.mandelbrotjavafx.tests;
+}
