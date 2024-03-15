decf29cde046317da236b9cee839aaa8a2ee1ac6
fijiro <jiro.protonmail@protonmail.com>
2024-03-15 16:30:07 +0200

    Add file methods

--- src/main/java/iiro/toiv/mandelbrotjavafx/Files/FileController.java
+++ src/main/java/iiro/toiv/mandelbrotjavafx/Files/FileController.java
@@ -1,5 +1,20 @@
 package iiro.toiv.mandelbrotjavafx.Files;
 
-public class FileController {
+import javafx.scene.paint.Color;
 
+import java.util.ArrayList;
+
+public class FileController {
+    public void savePalette(String path, ArrayList<Color> palette) {
+
+    }
+    public ArrayList<Color> readPalette(String path) {
+        return new ArrayList<>();
+    }
+    public void savePosition(String path, double xMin, double yMin, double dist) {
+
+    }
+    public void readPosition(String path) {
+
+    }
 }
