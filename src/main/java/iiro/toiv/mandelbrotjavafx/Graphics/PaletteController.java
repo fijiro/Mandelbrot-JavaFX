a555c2153fd2d00c6eb2c9b9de255b90fdf6d550
fijiro <jiro.protonmail@protonmail.com>
2024-03-19 22:59:06 +0200

    refactor color

--- src/main/java/iiro/toiv/mandelbrotjavafx/Graphics/PaletteController.java
+++ src/main/java/iiro/toiv/mandelbrotjavafx/Graphics/PaletteController.java
@@ -5,12 +5,12 @@ import javafx.collections.ObservableList;
 import javafx.scene.paint.Color;
 
 import java.io.Serializable;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class PaletteController implements Serializable {
     public enum PaletteType {BlueToWhite, BlueToYellow, BlueToWhiteToYellow}
-    private ArrayList<Color> palette = new ArrayList<>();
+
+    private transient ArrayList<Color> palette = new ArrayList<>();
     private double colorSpeed = 32;
 
     public Color getPaletteColor(double mu) {
@@ -19,14 +19,26 @@ public class PaletteController implements Serializable {
         //default to black
         if (palette.isEmpty() || colorDistance == 1) return new Color(0, 0, 0, 1);
 
-        double distanceInPalette = (palette.size() - 1) * colorDistance;
-        Color paletteColor1 = palette.get((int) Math.floor(distanceInPalette));
-        Color paletteColor2 = palette.get((int) Math.ceil(distanceInPalette));
-        distanceInPalette -= Math.floor(distanceInPalette);
-
-        return new Color((paletteColor2.getRed() - paletteColor1.getRed()) * distanceInPalette + paletteColor1.getRed(),
-                (paletteColor2.getGreen() - paletteColor1.getGreen()) * distanceInPalette + paletteColor1.getGreen(),
-                (paletteColor2.getBlue() - paletteColor1.getBlue()) * distanceInPalette + paletteColor1.getBlue(), 1);
+        // Assign the point of how far in palette the color is, from 0 - palette.size()
+        double distanceInPalette = (palette.size()) * colorDistance;
+        Color pColor2 = null;
+        Color pColor1 = null;
+        try {
+            if(distanceInPalette > palette.size() - 1) {
+                pColor1 = palette.getLast();
+                pColor2 = palette.getFirst();
+            } else {
+                pColor1 = palette.get((int) Math.floor(distanceInPalette % (palette.size())));
+                pColor2 = palette.get((int) Math.ceil(distanceInPalette % (palette.size())));
+            }
+        } catch (Exception e) {
+            System.out.println("dist: " + distanceInPalette + " " + e);
+        }
+        colorDistance = distanceInPalette - Math.floor(distanceInPalette);
+        if(pColor2 == null || pColor1 == null) {
+            return new Color(1, 0.1, 1, 1);
+        }
+        return new Color((pColor2.getRed() - pColor1.getRed()) * colorDistance + pColor1.getRed(), (pColor2.getGreen() - pColor1.getGreen()) * colorDistance + pColor1.getGreen(), (pColor2.getBlue() - pColor1.getBlue()) * colorDistance + pColor1.getBlue(), 1);
     }
 
     // Clears palette and generates new one
@@ -36,13 +48,11 @@ public class PaletteController implements Serializable {
             case BlueToWhite:
                 palette.add(new Color(0, 0, 1, 1));
                 palette.add(new Color(1, 1, 1, 1));
-                palette.add(new Color(0, 0, 1, 1));
                 break;
 
             case BlueToYellow:
                 palette.add(new Color(0, 0, 1, 1));
                 palette.add(new Color(1, 1, 0, 1));
-                palette.add(new Color(0, 0, 1, 1));
                 break;
 
             case BlueToWhiteToYellow:
@@ -51,27 +61,32 @@ public class PaletteController implements Serializable {
                 palette.add(new Color(1, 1, 1, 1));
                 palette.add(new Color(1, 1, 0, 1));
                 palette.add(new Color(1, 0, 0, 1));
-                palette.add(new Color(0.08, 0.08, 0.08, 1));
+                palette.add(new Color(0, 1, 0, 1));
                 break;
 
             default:
                 break;
         }
     }
+
     public void setPalette(ArrayList<Color> palette) {
         this.palette = palette;
     }
-    public ObservableList<Color> getPalette() {
-        return FXCollections.observableList(palette);
+
+    public ArrayList<Color> getPalette() {
+        return palette;
     }
 
     public void addColor(Double r, Double g, Double b) {
-        palette.removeFirst();
-        palette.add(new Color(r,g,b,1));
-        palette.addFirst(new Color(r,g,b,1));
+        palette.add(new Color(r, g, b, 1));
     }
+
+    public void removeColor(int index) {
+        palette.remove(index);
+    }
+
     public void adjustSpeed(double adjustment) {
         colorSpeed += adjustment;
-        colorSpeed = Math.max(colorSpeed,0);
+        colorSpeed = Math.max(colorSpeed, 0);
     }
 }
