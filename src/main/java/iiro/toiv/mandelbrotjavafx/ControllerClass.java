e64a1fd7da76ea2d66df76e50322a59b85a0cb55
fijiro <77334716+fijiro@users.noreply.github.com>
2024-04-11 10:22:41 +0300

    Add abstract class

---
+++ src/main/java/iiro/toiv/mandelbrotjavafx/ControllerClass.java
@@ -0,0 +1,8 @@
+package iiro.toiv.mandelbrotjavafx;
+
+public abstract class ControllerClass {
+    //Forces all controllers to have a toString() method. Why? Because the assignment requires an abstract class.
+    //Here we can also define other methods we want to be shared between controllers.
+
+    public abstract String toString();
+}
