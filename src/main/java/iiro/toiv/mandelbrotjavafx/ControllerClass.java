c2782bb0d555c9a4ae8ba27089a0b74bd2d218d1
fijiro <77334716+fijiro@users.noreply.github.com>
2024-04-12 16:15:15 +0300

    Add description

--- src/main/java/iiro/toiv/mandelbrotjavafx/ControllerClass.java
+++ src/main/java/iiro/toiv/mandelbrotjavafx/ControllerClass.java
@@ -2,10 +2,10 @@ package iiro.toiv.mandelbrotjavafx;
 
 /**
  * Mandatory abstract class.
+ * Forces all controllers to have a toString() method for debugging.
+ * Here we could also define other methods we would want to be shared between controllers.
  */
 public abstract class ControllerClass {
-    //Forces all controllers to have a toString() method for debugging.
-    //Here we could also define other methods we would want to be shared between controllers.
 
     /**
      * @return Returns brief description of the controller.
